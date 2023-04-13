479514af85685c6bfab9d39d8429ec999543c2c9
Tertius Erskine <terersk@gmail.com>
2023-04-12 20:00:34 -0400

    .

--- src/main/java/com/cpan228/ClothingStore/config/SecurityConfig.java
+++ src/main/java/com/cpan228/ClothingStore/config/SecurityConfig.java
@@ -1,15 +1,14 @@
 package com.cpan228.ClothingStore.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 import com.cpan228.ClothingStore.model.CustomUserService;
 
@@ -31,23 +30,25 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        
+
         http
-            .csrf()
-            .disable()
-            .httpBasic()
-            .and()
-            .authorizeHttpRequests()
-            .requestMatchers("/h2-console/**","login","/register","/logout").permitAll()
-            .requestMatchers("/","/add", "/itemslist/**","/manage/**").authenticated()
-            .and()
-            .formLogin()
-            .loginPage("/login")
-            .defaultSuccessUrl("/itemslist",true)
-            .permitAll()
-            .and()
-            .logout()
-            .logoutSuccessUrl("/login");
+                .authorizeHttpRequests()
+                .requestMatchers(toH2Console()).hasRole("ADMIN")
+                .requestMatchers("/login", "/register", "/logout").permitAll()
+                .requestMatchers("/", "/add", "/itemslist/**", "/manage/**").authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/itemslist", true)
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login")
+                .and()
+                .headers()
+                .frameOptions()
+                .disable();
+        http.csrf().disable();
         return http.build();
     }
 
